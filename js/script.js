a5ed168b276c64566894afad9634d8af0cb93ecc
Zurab-D <dzn@inbox.com>
2016-04-22 22:54:59 +0300

    + gh-pages

---
+++ js/script.js
@@ -0,0 +1,42 @@
+(function () {
+  var mapHtmlElement = document.getElementById("map");
+
+  function initialize() {
+    var myLatLng = new google.maps.LatLng(34.8697366,-111.7607833);
+
+    var map = new google.maps.Map(mapHtmlElement, {
+      center: myLatLng,
+      scrollwheel: false,
+      zoom: 7,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      disableDefaultUI: true
+    });
+
+    var iconMarker = "img/map-marker.svg";
+
+    var marker = new google.maps.Marker({
+      position: myLatLng,
+      title: "Sedona",
+      icon: iconMarker
+    });
+
+    marker.setMap(map);
+  }
+
+  if (mapHtmlElement) {
+    google.maps.event.addDomListener(window, 'load', initialize);
+  }
+}());
+/**
+ * Мобильное меню
+ */
+(function(){
+  var
+    nav    = document.querySelector('.nav'),
+    navBtn = nav.querySelector('.nav__btn')
+  ;
+  nav.classList.remove("nav--expand");
+  navBtn.addEventListener("click", function() {
+    nav.classList.toggle("nav--expand");
+  });
+}())
